9b0fc0e5049d62e37d2d7b9b9fbf9101394142af
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-07-04 14:26:30 +0700

    Initial Commit

---
+++ TableViewHandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewHandsOn
+//
+//  Created by Bill Tanthowi Jauhari on 04/07/19.
+//  Copyright © 2019 Batavia Hack Town. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
